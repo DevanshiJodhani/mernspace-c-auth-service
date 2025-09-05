0d3ab79651bc40d17e88b86522e5255bcc812f24
devanshi <DevanshiJodhani>
2025-09-05 15:20:48 +0530

    add create tenant endpoint

--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -2,6 +2,7 @@ import { DataSource } from 'typeorm'
 import request from 'supertest'
 import app from '../../src/app'
 import { AppDataSource } from '../../src/config/data-source'
+import { Tenant } from '../../src/entity/Tenant'
 
 describe('POST /tenants', () => {
     let connection: DataSource
@@ -32,6 +33,23 @@ describe('POST /tenants', () => {
                 .send(tenantData)
 
             expect(response.statusCode)
+        })
+
+        it('Should create a tenant in the database', async () => {
+            const tenantData = {
+                name: 'Tenant name',
+                address: 'Tenant address',
+            }
+
+            await request(app).post('/tenants').send(tenantData)
+
+            const tenantRepository = connection.getRepository(Tenant)
+
+            const tenants = await tenantRepository.find()
+
+            expect(tenants).toHaveLength(1)
+            expect(tenants[0].name).toBe(tenantData.name)
+            expect(tenants[0].address).toBe(tenantData.address)
         })
     })
 })
