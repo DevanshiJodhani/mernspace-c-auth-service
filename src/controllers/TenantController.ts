550a19db039d8a55296cd6645d65ff36c358f117
devanshi <DevanshiJodhani>
2025-09-08 19:50:23 +0530

    done CURD operation on tenants

--- src/controllers/TenantController.ts
+++ src/controllers/TenantController.ts
@@ -1,7 +1,9 @@
-import { NextFunction, Response } from 'express'
+import { NextFunction, Response, Request } from 'express'
 import { TenantService } from '../services/TenantService'
-import { CreateTenantRequest } from '../types'
+import { CreateTenantRequest, TenantQueryParams } from '../types'
 import { Logger } from 'winston'
+import { matchedData, validationResult } from 'express-validator/check'
+import createHttpError from 'http-errors'
 
 export class TenantController {
     constructor(
@@ -10,16 +12,118 @@ export class TenantController {
     ) {}
 
     async create(req: CreateTenantRequest, res: Response, next: NextFunction) {
-        const { name, address } = req.body
+        // Validation
+        const result = validationResult(req)
+        if (!result.isEmpty()) {
+            return res.status(400).json({ errors: result.array() })
+        }
 
+        const { name, address } = req.body
         this.logger.debug('Request for creating a tenant', req.body)
 
         try {
             const tenant = await this.tenantService.create({ name, address })
-
             this.logger.info('Tenant has been created', { id: tenant.id })
 
             res.status(201).json({ id: tenant.id })
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    async getAll(req: Request, res: Response, next: NextFunction) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        const validatedQuery = matchedData(req, {
+            onlyValidData: true,
+        }) as TenantQueryParams
+        try {
+            const [tenants, count] =
+                await this.tenantService.getAll(validatedQuery)
+
+            this.logger.info('All tenant have been fetched')
+            res.json({
+                currentPage: validatedQuery.currentPage,
+                perPage: validatedQuery.perPage,
+                total: count,
+                data: tenants,
+            })
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                this.logger.error(err.message)
+            }
+            next(err)
+        }
+    }
+
+    async getOne(req: Request, res: Response, next: NextFunction) {
+        const tenantId = req.params.id
+
+        if (isNaN(Number(tenantId))) {
+            next(createHttpError(400, 'Invalid url param.'))
+            return
+        }
+
+        try {
+            const tenant = await this.tenantService.getById(Number(tenantId))
+
+            if (!tenant) {
+                next(createHttpError(400, 'Tenant does not exist.'))
+                return
+            }
+
+            this.logger.info('Tenant has been fetched')
+            res.json(tenant)
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    async update(req: CreateTenantRequest, res: Response, next: NextFunction) {
+        // Validation
+        const result = validationResult(req)
+        if (!result.isEmpty()) {
+            return res.status(400).json({ errors: result.array() })
+        }
+
+        const { name, address } = req.body
+        const tenantId = req.params.id
+
+        if (isNaN(Number(tenantId))) {
+            next(createHttpError(400, 'Invalid url param.'))
+            return
+        }
+
+        this.logger.debug('Request for updating a tenant', req.body)
+
+        try {
+            await this.tenantService.update(Number(tenantId), {
+                name,
+                address,
+            })
+
+            this.logger.info('Tenant has been updated', { id: tenantId })
+
+            res.json({ id: Number(tenantId) })
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    async destroy(req: Request, res: Response, next: NextFunction) {
+        const tenantId = req.params.id
+
+        if (isNaN(Number(tenantId))) {
+            next(createHttpError(400, 'Invalid url param.'))
+            return
+        }
+
+        try {
+            await this.tenantService.deleteById(Number(tenantId))
+
+            this.logger.info('Tenant has been deleted', {
+                id: Number(tenantId),
+            })
+            res.json({ id: Number(tenantId) })
         } catch (err) {
             next(err)
         }
--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -1,4 +1,9 @@
-import express from 'express'
+import express, {
+    Request,
+    Response,
+    NextFunction,
+    RequestHandler,
+} from 'express'
 import { TenantController } from '../controllers/TenantController'
 import { TenantService } from '../services/TenantService'
 import { AppDataSource } from '../config/data-source'
@@ -7,6 +12,9 @@ import logger from '../config/logger'
 import authenticate from '../middlewares/authenticate'
 import { canAccess } from '../middlewares/canAccess'
 import { Roles } from '../constants'
+import listUsersValidator from '../validators/list-users-validator'
+import tenantValidator from '../validators/tenant-validator'
+import { CreateTenantRequest } from '../types'
 
 const router = express.Router()
 
@@ -14,8 +22,46 @@ const tenantRepository = AppDataSource.getRepository(Tenant)
 const tenantService = new TenantService(tenantRepository)
 const tenantController = new TenantController(tenantService, logger)
 
-router.post('/', authenticate, canAccess([Roles.ADMIN]), (req, res, next) =>
-    tenantController.create(req, res, next),
+router.post(
+    '/',
+    authenticate as RequestHandler,
+    canAccess([Roles.ADMIN]),
+    tenantValidator,
+    (req: CreateTenantRequest, res: Response, next: NextFunction) =>
+        tenantController.create(req, res, next) as unknown as RequestHandler,
+)
+
+router.get(
+    '/',
+    listUsersValidator,
+    (req: Request, res: Response, next: NextFunction) => {
+        tenantController.getAll(req, res, next) as unknown as RequestHandler
+    },
+)
+
+router.get(
+    '/:id',
+    authenticate as RequestHandler,
+    canAccess([Roles.ADMIN]),
+    (req, res, next) =>
+        tenantController.getOne(req, res, next) as unknown as RequestHandler,
+)
+
+router.patch(
+    '/:id',
+    authenticate as RequestHandler,
+    canAccess([Roles.ADMIN]),
+    tenantValidator,
+    (req: CreateTenantRequest, res: Response, next: NextFunction) =>
+        tenantController.update(req, res, next) as unknown as RequestHandler,
+)
+
+router.delete(
+    '/:id',
+    authenticate as RequestHandler,
+    canAccess([Roles.ADMIN]),
+    (req, res, next) =>
+        tenantController.destroy(req, res, next) as unknown as RequestHandler,
 )
 
 export default router
