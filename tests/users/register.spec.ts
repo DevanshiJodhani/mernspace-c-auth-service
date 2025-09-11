e2a2943b115cb1f962f973343dc8c42e0c784f28
devanshi <DevanshiJodhani>
2025-09-12 01:28:36 +0530

    add: debug the env variables

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -10,14 +10,7 @@ import { RefreshToken } from '../../src/entity/RefreshToken'
 import dotenv from 'dotenv'
 dotenv.config()
 
-if (!process.env.CI) {
-    dotenv.config()
-}
-
-console.log(`DB_HOST: ${process.env.DB_HOST}`)
-console.log(`DB_PORT: ${process.env.DB_PORT}`)
-console.log(`DB_USERNAME: ${process.env.DB_USERNAME}`)
-console.log(`DB_PASSWORD: ${process.env.DB_PASSWORD}`)
+console.log(`DATABASE_URL: ${process.env.DATABASE_URL}`)
 console.log(`DB_NAME: ${process.env.DB_NAME}`)
 console.log(`REFRESH_TOKEN_SECRET: ${process.env.REFRESH_TOKEN_SECRET}`)
 console.log(`JWKS_URI: ${process.env.JWKS_URI}`)
