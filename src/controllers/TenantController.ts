d4c6f873b7bed65b25e495e37e29c7c265e98250
devanshijodhani <devanshijodhani45@gmai.com>
2026-01-10 00:21:14 +0530

    update: get all tenant function

--- src/controllers/TenantController.ts
+++ src/controllers/TenantController.ts
@@ -45,8 +45,6 @@ export class TenantController {
                 total: count,
                 data: tenants,
             })
-
-            res.json(tenants)
         } catch (err) {
             next(err)
         }
