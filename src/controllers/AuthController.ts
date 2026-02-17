b6a1d78c301f4591431bb8ab8b13da549cb82e02
devanshijodhani <devanshijodhani45@gmai.com>
2026-02-17 18:41:19 +0530

    update: token details

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -52,6 +52,9 @@ export class AuthController {
             const payload: JwtPayload = {
                 sub: String(user.id),
                 role: user.role,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
             }
 
             const accessToken = this.tokenService.generateAccessToken(payload)
@@ -137,6 +140,9 @@ export class AuthController {
                 sub: String(user.id),
                 role: user.role,
                 tenant: user.tenant ? String(user.tenant.id) : '',
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
             }
 
             const accessToken = this.tokenService.generateAccessToken(payload)
